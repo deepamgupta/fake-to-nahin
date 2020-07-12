2fe4b60cf05c4534962edb581f2bb5da4a36f459
nilayostwal999 <nilay.ostwal999@gmail.com>
2020-07-12 19:50:21 +0530

    profile edit screen problems resolved

--- lib/screens/ProfileEditScreen.dart
+++ lib/screens/ProfileEditScreen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../globals.dart' as globals;
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -8,6 +10,7 @@ class ProfileEditScreen extends StatefulWidget {
 }
 
 class _ProfileEditScreenState extends State<ProfileEditScreen> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +20,11 @@ class _ProfileEditScreenState extends State<ProfileEditScreen> {
           actions: [
             RaisedButton(
               onPressed: () {
-                Navigator.pop(context);
+                _formKey.currentState.save();
+                createUser().then((value) => {
+                      Navigator.pop(context),
+                      Navigator.popAndPushNamed(context, 'Profile')
+                    });
               },
               child: Row(children: [
                 Icon(Icons.save),
@@ -29,153 +36,187 @@ class _ProfileEditScreenState extends State<ProfileEditScreen> {
             )
           ],
         ),
-        body: ListView(
-            padding: EdgeInsets.fromLTRB(5, 20, 5, 20),
-            scrollDirection: Axis.vertical,
-            children: [
-              Card(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child: Text('First Name:',
-                              style: TextStyle(fontSize: 20))),
-                      Expanded(
-                          flex: 5,
-                          child: TextFormField(
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            keyboardType: TextInputType.text,
-                            initialValue: globals.currentUser.firstName,
-                            cursorWidth: 2,
-                          )),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  )),
-              Card(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child: Text('Last Name:',
-                              style: TextStyle(fontSize: 20))),
-                      Expanded(
-                          flex: 5,
-                          child: TextFormField(
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            keyboardType: TextInputType.text,
-                            initialValue: globals.currentUser.lastName,
-                            cursorWidth: 2,
-                          )),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  )),
-              Card(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child: Text('Username:',
-                              style: TextStyle(fontSize: 20))),
-                      Expanded(
-                          flex: 5,
-                          child: TextFormField(
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            keyboardType: TextInputType.text,
-                            initialValue: globals.currentUser.username,
-                            cursorWidth: 2,
-                          )),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  )),
-              Card(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child: Text('City:', style: TextStyle(fontSize: 20))),
-                      Expanded(
-                          flex: 5,
-                          child: TextFormField(
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            keyboardType: TextInputType.text,
-                            initialValue: globals.currentUser.city,
-                            cursorWidth: 2,
-                          )),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  )),
-              Card(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child:
-                              Text('State:', style: TextStyle(fontSize: 20))),
-                      Expanded(
-                          flex: 5,
-                          child: TextFormField(
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            keyboardType: TextInputType.text,
-                            initialValue: globals.currentUser.state,
-                            cursorWidth: 2,
-                          )),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  )),
-              Card(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child:
-                              Text('Country:', style: TextStyle(fontSize: 20))),
-                      Expanded(
-                          flex: 5,
-                          child: TextFormField(
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            keyboardType: TextInputType.text,
-                            initialValue: globals.currentUser.country,
-                            cursorWidth: 2,
-                          )),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  )),
-              Card(
-                  margin: EdgeInsets.fromLTRB(5, 10, 5, 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child: Text('Mobile No.:',
-                              style: TextStyle(fontSize: 20))),
-                      Expanded(
-                          flex: 5,
-                          child: TextFormField(
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                            keyboardType: TextInputType.phone,
-                            initialValue: globals.currentUser.mobile,
-                            cursorWidth: 2,
-                          )),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  )),
-            ]));
+        body: Form(
+            key: _formKey,
+            child: ListView(
+                padding: EdgeInsets.fromLTRB(5, 20, 5, 20),
+                scrollDirection: Axis.vertical,
+                children: [
+                  Card(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Text('First Name:',
+                                  style: TextStyle(fontSize: 20))),
+                          Expanded(
+                              flex: 5,
+                              child: TextFormField(
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                  keyboardType: TextInputType.text,
+                                  initialValue: globals.currentUser.firstName,
+                                  cursorWidth: 2,
+                                  onSaved: (value) {
+                                    globals.currentUser.firstName =
+                                        value.trim();
+                                  })),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      )),
+                  Card(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Text('Last Name:',
+                                  style: TextStyle(fontSize: 20))),
+                          Expanded(
+                              flex: 5,
+                              child: TextFormField(
+                                  initialValue: globals.currentUser.lastName,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                  keyboardType: TextInputType.text,
+                                  cursorWidth: 2,
+                                  onSaved: (value) {
+                                    globals.currentUser.lastName = value.trim();
+                                  })),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      )),
+                  Card(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Text('Username:',
+                                  style: TextStyle(fontSize: 20))),
+                          Expanded(
+                              flex: 5,
+                              child: TextFormField(
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                  keyboardType: TextInputType.text,
+                                  initialValue: globals.currentUser.username,
+                                  cursorWidth: 2,
+                                  onSaved: (value) {
+                                    globals.currentUser.username = value.trim();
+                                  })),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      )),
+                  Card(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Text('City:',
+                                  style: TextStyle(fontSize: 20))),
+                          Expanded(
+                              flex: 5,
+                              child: TextFormField(
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                  keyboardType: TextInputType.text,
+                                  initialValue: globals.currentUser.city,
+                                  cursorWidth: 2,
+                                  onSaved: (value) {
+                                    globals.currentUser.city = value.trim();
+                                  })),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      )),
+                  Card(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Text('State:',
+                                  style: TextStyle(fontSize: 20))),
+                          Expanded(
+                              flex: 5,
+                              child: TextFormField(
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                  keyboardType: TextInputType.text,
+                                  initialValue: globals.currentUser.state,
+                                  cursorWidth: 2,
+                                  onSaved: (value) {
+                                    globals.currentUser.state = value.trim();
+                                  })),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      )),
+                  Card(
+                      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Text('Country:',
+                                  style: TextStyle(fontSize: 20))),
+                          Expanded(
+                              flex: 5,
+                              child: TextFormField(
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                  keyboardType: TextInputType.text,
+                                  initialValue: globals.currentUser.country,
+                                  cursorWidth: 2,
+                                  onSaved: (value) {
+                                    globals.currentUser.country = value.trim();
+                                  })),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      )),
+                  Card(
+                      margin: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Text('Mobile No.:',
+                                  style: TextStyle(fontSize: 20))),
+                          Expanded(
+                              flex: 5,
+                              child: TextFormField(
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                  keyboardType: TextInputType.phone,
+                                  initialValue: globals.currentUser.mobile,
+                                  cursorWidth: 2,
+                                  onSaved: (value) {
+                                    globals.currentUser.mobile = value.trim();
+                                  })),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      )),
+                ])));
   }
 
   String capitalize(word) {
     return "${word[0].toUpperCase()}${word.substring(1)}";
+  }
+
+  Future createUser() async {
+    var userMap = globals.currentUser.toMap();
+    final db = Firestore.instance;
+    await db
+        .collection("users")
+        .document(globals.currentUser.email)
+        .setData(userMap);
   }
 }
